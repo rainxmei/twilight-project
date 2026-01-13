d68b22a2f863aa9186b25f4de9678d918cb0462c
Abyan <abyan.keane@gmail.com>
2026-01-13 20:35:25 +0700

    update progres

--- progres-januari/06-memory-address/src/main.cpp
+++ progres-januari/06-memory-address/src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 
 using namespace std;
+
+using Clock = std::chrono::high_resolution_clock;
+using TimePoint = std::chrono::time_point<Clock>;
 
 class Kosong {
     public:
@@ -39,21 +44,39 @@ int main(int argc, char const *argv[])
     string a;
     string b;
     Mahasiswa object1 = Mahasiswa("abyan");
+    Mahasiswa* object2 = new Mahasiswa("budi");
     string c;
     string d;
     cout << "\nAddress stack" << endl;
     cout << &a << endl;
     cout << &b << endl;
     cout << &object1 << endl;
+    cout << &object2 << endl;
+    cout << object2 << endl;
     cout << &c << endl;
     cout << &d << endl;
+ 
+    // seberapa cepat kita membuat object di masing-masing memory
 
-    cout << "\nAddress heap" << endl;
-    string a;
-    string b;
-    Mahasiswa* object2 = new Mahasiswa("budi");
-    string c;
-    string d;
+    TimePoint *tStackStart = new TimePoint();
+    TimePoint *tStackEnd = new TimePoint();
+    TimePoint *tHeapStart = new TimePoint();
+    TimePoint *tHeapEnd = new TimePoint();
+
+    cin.get();
+    *tStackStart = Clock::now();
+    Mahasiswa object3 = Mahasiswa("abyan");
+    *tStackEnd = Clock::now();
+
+    cin.get();
+    *tHeapStart = Clock::now();
+    Mahasiswa* object4 = new Mahasiswa("budi");
+    *tHeapEnd = Clock::now();
+
+    chrono::duration<double> durasi = *tStackEnd - *tStackStart;
+    cout << "Waktu pembuatan object di stack adalah : " << durasi.count() << endl;
+    durasi = *tHeapEnd - *tHeapStart;
+    cout << "Waktu pembuatan object di heap adalah : " << durasi.count() << endl;
 
     return 0;
 }
