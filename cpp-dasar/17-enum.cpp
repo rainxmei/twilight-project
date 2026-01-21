197be8e5120f511ce08393946f61723240c0b3f7
Abyan <abyan.keane@gmail.com>
2026-01-21 12:53:41 +0700

    update progres

---
+++ cpp-dasar/17-enum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+enum warna{merah, putih, hitam, coklat = 5, kuning, biru};
+
+int main(){
+    warna kain;
+
+    kain = hitam;
+
+    if (kain == hitam){
+        cout << "warna kain hitam" << endl;
+    }
+
+    cout << kain << endl;
+
+    return 0;
+}
