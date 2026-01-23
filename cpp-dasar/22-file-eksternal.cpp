1f4365f65ff38031546d3415c363ca810fba5299
Abyan <abyan.keane@gmail.com>
2026-01-23 19:46:07 +0700

    update progres

---
+++ cpp-dasar/22-file-eksternal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    
+    ofstream myFile;
+
+    // ios::out = default, operasi output;
+    // ios::app = menuliskan pada akhir baris;
+    // ios::trunc = default, membuat file jika tidak ada dan menghapus jika ada
+
+    myFile.open("data3.txt", ios::app); // append
+    myFile << "\nmenuliskan baris baru pada data3";
+    myFile.close();
+
+    myFile.open("data1.txt", ios::out);
+    myFile << "\nmenuliskan baris baru pada data1";
+    myFile.close();
+
+    myFile.open("data2.txt", ios::trunc);
+    myFile << "\nmenuliskan baris baru pada data2";
+    myFile.close();
+
+    return 0;
+}
