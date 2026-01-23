12defaaf0b1d14ae8780ce234ac099ce3cf5d6a8
Abyan <abyan.keane@gmail.com>
2026-01-23 19:25:35 +0700

    update progres

---
+++ cpp-dasar/21-casting-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 5;
+    float b = 6.67f;
+    char c = 'd';
+
+    cout << a + (int)b << endl;
+
+    cout << (char)(c + a) << endl;
+
+    return 0;
+}
