dcf36f3c1316e2cac301f3b03bd002abd16c18ee
Abyan <abyan.keane@gmail.com>
2026-02-04 09:49:03 +0700

    update progres

--- studi-kasus/09-grade-school.cpp
+++ studi-kasus/09-grade-school.cpp
@@ -2,12 +2,55 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <algorithm>
+#include <stdexcept>
 
-namespace school{
-    
-    void 
-}
+namespace School
+{
+    std::map<int, std::vector<std::string>> student;
 
-int main(){
+    void add(const std::string &name, int grade)
+    {
+        auto &students = student[grade];
 
+        for (const auto &[x, students] : student)
+        {
+            if (std::find(students.begin(), students.end(), name) != students.end())
+            {
+                throw std::runtime_error("Student sudah ada");
+            }
+        }
+
+        students.push_back(name);
+        std::sort(students.begin(), students.end());
+    }
+
+    std::vector<std::string> roster_all()
+    {
+        std::vector<std::string> result;
+
+        for (const auto &[x, students] : student)
+        {
+            result.insert(result.end(), students.begin(), students.end());
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    School::add("Jim", 2);
+    School::add("Anna", 1);
+    School::add("Barb", 1);
+    School::add("Charlie", 1);
+    School::add("Alex", 2);
+    School::add("Peter", 2);
+    School::add("Zoe", 2);
+    // School::add("Jim", 5);
+
+    for (auto name : School::roster_all())
+    {
+        std::cout << name << " ";
+    }
 }
