cd98fa88710ac6e94e989a31f9d1a257d545c46a
Abyan <abyan.keane@gmail.com>
2026-02-04 16:15:47 +0700

    update progres

--- studi-kasus/15-nth-prime.cpp
+++ studi-kasus/15-nth-prime.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
+#include <stdexcept>
+#include <cmath>
 
 namespace prime
 {
-    std::string 
+    int nth_prime(int val)
+    {
+        int count = 0;
+        int angka = 2;
+
+        if (val <= 0)
+        {
+            throw std::invalid_argument(" harus positif");
+        }
+
+        while (count != val)
+        {
+            bool isPrime = true;
+
+            if (angka % 2 == 0 && angka != 2)
+            {
+                angka++;
+                continue;
+            }
+
+            for (int i = 2; i * i <= angka; i++)
+            {
+                if (angka % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                count++;
+            }
+            angka++;
+        }
+        return angka - 1;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-
+    std::cout << prime::nth_prime(3);
     return 0;
 }
