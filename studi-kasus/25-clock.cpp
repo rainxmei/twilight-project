03766e4b65d71778814e869e2d300530136a0951
Abyan <abyan.keane@gmail.com>
2026-02-16 16:56:13 +0700

    update progres

---
+++ studi-kasus/25-clock.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+class Clock
+{
+public:
+    int hour;
+    int minutes;
+
+    Clock(int hour, int minutes)
+    {
+        Clock::hour = hour;
+        Clock::minutes = minutes;
+    }
+
+    void add(int hourVal, int minuteVal)
+    {
+        hour += hourVal;
+        minutes += minuteVal;
+    }
+
+    void substrack(int hourVal, int minuteVal)
+    {
+        hour -= hourVal;
+        minutes -= minuteVal;
+    }
+
+    void printClock()
+    {
+        int totalMinutes;
+
+        totalMinutes = (hour * 60) + minutes;
+        totalMinutes = ((totalMinutes % 1440) + 1440) % 1440;
+
+        hour = totalMinutes / 60;
+        minutes = totalMinutes % 60;
+
+        std::string hourStr = std::to_string(hour);
+        std::string minuteStr = std::to_string(minutes);
+
+        if (hourStr.size() == 1)
+        {
+            hourStr = "0" + hourStr;
+        }
+
+        if (minuteStr.size() == 1)
+        {
+            minuteStr = "0" + minuteStr;
+        }
+
+        std::cout << hourStr << ":" << minuteStr;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Clock jam1(9, 5);
+    jam1.add(0, 60);
+    jam1.printClock();
+    return 0;
+}
